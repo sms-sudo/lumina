c21db9e07046f96632144eb823023a432f9a7f19
sms-sudo <56768229+sms-sudo@users.noreply.github.com>
2025-07-05 22:50:38 -0400

    Update 4_📅_Visualizations (Comparison).py

--- pages/4_📅_Visualizations (Comparison).py
+++ pages/4_📅_Visualizations (Comparison).py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Visualizations (Comparison)", layout="wide")
 st.title("📅 Visualizations (Comparison)")
 
 # Check if "loaded_data" exists and is not empty
-if "loaded_data" not in st.session_state or st.session_state["loaded_data"] is None or st.session_state["loaded_data"].empty:
+if "loaded_data" not in st.session_state or st.session_state["loaded_data"] is None:
     st.info("ℹ️ No data loaded. Please navigate to Raw Data Validation (on the left hand selection).")
 else:
     loaded_data = st.session_state["loaded_data"]
